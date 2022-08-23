c7abe713e7a5c8ca226567e18ce920dc5f7ce507
jeffmgranja <jgranja.stanford@gmail.com>
2022-08-23 13:01:28 -0700

    Update global_defaults.R

--- R/global_defaults.R
+++ R/global_defaults.R
@@ -1,20 +1,18 @@
 jg_logo <- "
-    _             _       _   ____  
-   (_) __ _ _ __ | | ___ | |_|___ \\ 
-   | |/ _` | '_ \\| |/ _ \\| __| __) |
-   | | (_| | |_) | | (_) | |_ / __/ 
-  _/ |\\__, | .__/|_|\\___/ \\__|_____|
- |__/ |___/|_|                      
+   _                               
+  (_) __ ___   _____ _ __ ___  ___ 
+  | |/ _` \\ \\ / / _ \\ '__/ __|/ _ \\
+  | | (_| |\\ V /  __/ |  \\__ \\  __/
+ _/ |\\__, | \\_/ \\___|_|  |___/\\___|
+|__/ |___/                         
 "
 
 jg_dependency <- c(
-  "parallel",
-  "jgplot2"
+  "parallel"
 )
 
 jg_github <- c(
-  "zeehio/facetscales",
-  "caleblareau/BuenColors"
+  "jeffmgranja/jgplot2"
 )
 
 .onAttach <- function(libname, pkgname){
@@ -23,9 +21,9 @@ jg_github <- c(
   packageStartupMessage("Logo created from patorjk.com")
 
   #Package Startup
-  v <- packageVersion("jgplot2")
-  packageStartupMessage("jgplot2 : Version ", v)
-  packageStartupMessage("jgplot2 website : https://github.com/jeffmgranja/jgplot2")
+  v <- packageVersion("jgverse")
+  packageStartupMessage("jgverse : Version ", v)
+  packageStartupMessage("jgverse website : https://github.com/jeffmgranja/jgverse")
   
   #Load Packages
   packageStartupMessage("Loading Required Packages...")
@@ -33,7 +31,7 @@ jg_github <- c(
   for(i in seq_along(pkgs)){
     tryCatch({
       packageStartupMessage("\tLoading Package : ", pkgs[i], " v", packageVersion(pkgs[i]))
-      suppressPackageStartupMessages(require(pkgs[i], character.only=TRUE))
+      .quiet(suppressPackageStartupMessages(require(pkgs[i], character.only=TRUE)))
     }, error = function(e){
       packageStartupMessage("\tFailed To Load Package : ", pkgs[i])
     })
@@ -44,7 +42,7 @@ jg_github <- c(
   for(i in seq_along(pkgs)){
     tryCatch({
       packageStartupMessage("\tLoading Package : ", pkgs[i], " v", packageVersion(pkgs[i]))
-      suppressPackageStartupMessages(require(pkgs[i], character.only=TRUE))
+      .quiet(suppressPackageStartupMessages(require(pkgs[i], character.only=TRUE)))
     }, error = function(e){
       packageStartupMessage("\tFailed To Load Github Package : devtools::install_github('", jg_github[i], "')")
       packageStartupMessage("\t\tAttempting to install...")
@@ -52,11 +50,13 @@ jg_github <- c(
     })
   }
 
-  #Set Defaults
-  jgplot2()
-
-  #Set Grid
-  set_grid_mm()
-
 }
 
+.quiet <- function(x) { 
+  tmp <- tempfile()
+  sink(tmp) 
+  on.exit(sink()) 
+  invisible(force(x)) 
+  if(file.exists(tmp)) file.remove(tmp)
+  0
+} 
